1e080649746808f08649610a0fe3466067a69596
qikke <qikke1995@126.com>
2018-08-02 19:17:06 +0800

    组件化

--- src/js/component/app-aside.js
+++ src/js/component/app-aside.js
@@ -3,31 +3,14 @@ Vue.component('app-aside',{
   template:`
     <div class="sidebar" v-if="mode === 'edit'">
       <div class="top">
-        <button @click="onClickSave">保存</button>
-        <button @click="share">分享</button>
-        <button @click="print">打印</button>
-        <button @click="skinChange">换肤</button>
+        <button @click="$emit('onclicksave')">保存</button>
+        <button @click="$emit('share')">分享</button>
+        <button @click="window.print()">打印</button>
+        <button @click="$emit('skinchange')">换肤</button>
       </div>
       <div class="bottom">
-        <button @click="onLogOut">登出</button>
+        <button @click="$emit('onlogout')">登出</button>
       </div>
     </div>
   `,
-  methods:{
-    onClickSave(){
-      this.$emit('onclicksave')
-    },
-    share(){
-      this.$emit('share')
-    },
-    print() {
-      window.print()
-    },
-    skinChange(){
-      this.$emit('skinchange')
-    },
-    onLogOut(){
-      this.$emit('onlogout')
-    }
-  }
 })
